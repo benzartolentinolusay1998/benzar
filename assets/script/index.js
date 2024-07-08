e776c4f13e1f0e3ef5bf5d3234ca76b99f68f48f
benzartolentinolusay1998 <benzartolentinolusay1998>
2024-07-08 17:39:57 +0800

    commit

---
+++ assets/script/index.js
@@ -0,0 +1,17 @@
+/*const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanbye = document.querySelector("#bye")
+
+
+console.log(spanHi.dataset)*/
+
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanHi2 = document.getElementById("hi2")
+const spanBye = document.querySelector("#bye")
+
+spanHi.classList.add("new-class")
+spanHi.classList.remove("hi1")
+
+spanHi.style.color = "red"
+spanHi.style.backgroundColor = "red"
